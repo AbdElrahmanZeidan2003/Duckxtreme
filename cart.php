b6b53ac6ae7b0a395b6d52d586adc48304bcd91e
AbdElrahmanZeidan2003 <130861647+AbdElrahmanZeidan2003@users.noreply.github.com>
2026-04-29 14:31:52 -0500

    logo

--- cart.php
+++ cart.php
@@ -11,7 +11,7 @@
 
     <div class="navbar">
         <div class="logo">
-            <img src="picture/Logo.png" alt="DuckXtreme Logo">
+            <img src="picture/nLogo.png" alt="DuckXtreme Logo">
         </div>
 
         <nav>
